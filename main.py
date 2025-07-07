504a3b0aa72dac576ab70b4222b66a4576421480
matthieu-creator <matthieu.marechal2@gmail.com>
2025-07-07 09:51:56 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+
+# uvicorn main:app --host 127.0.0.1 --port 8000 --reload
+
+
+
+# Initialiser FastAPI
+description='Hello world'
+
+app = FastAPI(title="Word counter",description=description)
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+@app.get('/')
+async def index():
+    message = "This API counts the number of words in a text"
+    return message  
+
+@app.post("/count")
+async def count(text):
+    text = " ".join(text.strip().split('\t'))
+    text = " ".join(text.strip().split('\n'))
+    list_ = text.strip().split(' ')
+    is_space = True
+    while is_space:
+        is_space = False 
+        for elt in list_:
+           if elt == '':
+              list_.remove(elt)
+              is_space = True
+            
+    return len(list_) 
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
