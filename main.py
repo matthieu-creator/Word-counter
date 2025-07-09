e4f15fab088a5ad987b5bb11d830f216f1fd4485
matthieu-creator <matthieu.marechal2@gmail.com>
2025-07-08 22:26:51 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 # uvicorn main:app --host 127.0.0.1 --port 8000 --reload
 
 
 
-# Initialiser FastAPI
-description='Hello world'
 
-app = FastAPI(title="Word counter",description=description)
+app = FastAPI(
+    title="Word Counter",
+    description="API that counts the number of words in a given text"
+)
 
-from fastapi.middleware.cors import CORSMiddleware
-
+# CORS configuration
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
@@ -19,27 +20,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Pydantic model
+class TextInput(BaseModel):
+    text: str
 
-
-@app.get('/')
+@app.get("/")
 async def index():
-    message = "This API counts the number of words in a text"
-    return message  
+    return {"message": "This API counts the number of words in a text"}
 
 @app.post("/count")
-async def count(text):
-    text = " ".join(text.strip().split('\t'))
-    text = " ".join(text.strip().split('\n'))
-    list_ = text.strip().split(' ')
-    is_space = True
-    while is_space:
-        is_space = False 
-        for elt in list_:
-           if elt == '':
-              list_.remove(elt)
-              is_space = True
-            
-    return {"word_count":len(list_)} 
+async def count_words(input: TextInput):
+    words = input.text.split()
+    return {"word_count": len(words)}
 
 if __name__ == "__main__":
     import uvicorn
